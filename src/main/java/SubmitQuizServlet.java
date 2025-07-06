e122002a4cc46c7aea86dfe945cfa139a4f8124f
m-2404 <muskanrai2409@gmail.com>
2025-07-07 00:50:30 +0530

    Update SubmitQuizServlet.java

--- src/main/java/SubmitQuizServlet.java
+++ src/main/java/SubmitQuizServlet.java
@@ -7,9 +7,9 @@ import javax.servlet.http.*;
 
 @WebServlet("/SubmitQuizServlet")
 public class SubmitQuizServlet extends HttpServlet {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/quiz_app";
-    private static final String DB_USER = "root";
-    private static final String DB_PASS = "1234";
+    private static final String DB_URL = "jdbc:mysql://localhost:xxxx/quiz_app";
+    private static final String DB_USER = "db_user";
+    private static final String DB_PASS = "db_password";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
